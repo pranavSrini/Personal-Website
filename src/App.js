a47f63f4726e235545606c124969615890e313e7
pranavSrini <pranavs389@gmail.com>
2025-08-16 18:30:13 -0500

    Fix routing: Added basename to BrowserRouter for proper GitHub Pages routing and active tab detection

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/Personal-Website">
       <div className="App">
         <Navbar />
         <Routes>
